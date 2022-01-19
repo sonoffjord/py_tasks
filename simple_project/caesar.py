0042b2213593a184c36473f49717e8d15d15ed66
Aleksandr Repin <sonoffjord@gmail.com>
2022-01-19 20:22:13 +0300

    complite cesar

--- simple_project/caesar.py
+++ simple_project/caesar.py
@@ -1,18 +1,36 @@
-# def encryption_cesar(text, step, lang):
-#     encrypt_text = ''
-#     if lang == 'ru':
-#         for i in text:
+def encrypt_decrypt(text, step, lang, direction):
+    eng_lower_alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    eng_upper_alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    rus_lower_alphabet = "абвгдежзийклмнопрстуфхцчшщъыьэюя"
+    rus_upper_alphabet = "АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+    encrypt_string = ''
 
-
-def decryption_cesar(text, step, lang):
-    dencrypt_text = ''
+    if lang == 'ru':
+        alphas = 32
+        low_alpha = rus_lower_alphabet
+        upper_alpha = rus_upper_alphabet
     if lang == 'en':
-        for i in range(len(text)):
-            n = ord(text[i]) - step
-            if n < 97:
-                n = 122 - (96 - n)
-            dencrypt_text+=chr(n)
-    return dencrypt_text
+        alphas = 26
+        low_alpha = eng_lower_alphabet
+        upper_alpha = eng_upper_alphabet
 
+    for i in text:
+        if i.isalpha():
+            if i == i.lower():
+                place = low_alpha.index(i)
+            if i == i.upper():
+                place = upper_alpha.index(i)
 
-print(decryption_cesar('bwfusvfupdbftbs', 1, 'en'))
+            if direction == 'encrypt':
+                index = (place - int(step)) % alphas
+            elif direction == 'decrypt':
+                index = (place + int(step)) % alphas
+
+            if i == i.lower():
+                encrypt_string += low_alpha[index]
+            if i == i.upper():
+                encrypt_string += upper_alpha[index]
+
+        else:
+            encrypt_string += i
+    return encrypt_string
